67da29e1609472dfd71012bcddea95f6a1fd2055
Rodrigo Pereira <roddynle26@gmail.com>
2024-10-24 01:20:09 -0300

    First Project

---
+++ task.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What the city you're born ?\n")
+
+pet = input("What is your pet name ?\n")
+
+print ("What about " + city + " " + pet + " ?")
